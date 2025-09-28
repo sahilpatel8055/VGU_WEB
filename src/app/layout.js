e29201a6a575442646e3db396dfb381ecbf627da
sahil patel <104021781+sahilps@users.noreply.github.com>
2025-09-28 12:47:40 +0530

    Fifth commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,20 +9,48 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/ico" href="/favicon-vgu.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Online VGU – Dynamic Online Degree</title>
+<<<<<<< HEAD
 
         {/* Load CSS files from /public - matching the HTML structure */}
+=======
+        <meta name="description" content="Online VGU offers dynamic online degree programs including MBA, BBA, BCA, MCA, BA, MA, and M.Sc. courses with flexible learning options." />
+        <meta name="keywords" content="online degree, online MBA, online BBA, online BCA, online MCA, online education, VGU" />
+        
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        
+        {/* Load CSS files from /public - matching the HTML structure */}
+        <link rel="stylesheet" href="/css/inline_styles.css" />
+>>>>>>> 88757ed (First commit)
       </head>
       <body>
         <div id="root">
           {children}
         </div>
 
+<<<<<<< HEAD
         {/* Google tag (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js"
           async
           strategy="afterInteractive"
         />
+=======
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GA_MEASUREMENT_ID');
+          `}
+        </Script>
+>>>>>>> 88757ed (First commit)
         
         {/* Meta Pixel Code */}
         <noscript>
@@ -30,7 +58,15 @@ export default function RootLayout({ children }) {
         </noscript>
         <iframe height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
         
+<<<<<<< HEAD
         <div id="huntr-react-container-2"></div>
+=======
+        {/* Additional tracking scripts */}
+        <div id="huntr-react-container-2"></div>
+        
+        {/* Inline scripts */}
+        <Script src="/js/inline_scripts.js" strategy="afterInteractive" />
+>>>>>>> 88757ed (First commit)
       </body>
     </html>
   );
